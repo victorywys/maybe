04d054f25a5473206021c8c96b0c514d149b8113
victorywys <thisiswys@163.com>
2023-09-05 07:47:45 +0000

    seal to package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='maybee',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your package's dependencies here, e.g.,
+        # 'numpy>=1.10',
+    ],
+    # other metadata like author, license, etc.
+    author='RiichiUra3',
+    author_email='your.email@example.com',
+    description='A brief description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
