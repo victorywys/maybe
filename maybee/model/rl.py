b4f65b2e1b8b830e5113dfec66cd8c579ba67003
DQ Han <hdqhdq58@outlook.com>
2024-04-08 14:26:29 +0800

    Update rl.py

--- maybee/model/rl.py
+++ maybee/model/rl.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 import numpy as np
 import warnings
 import time
+import logging
 
 import torch
 import torch.nn as nn
@@ -102,14 +103,14 @@ class RLMahjong(nn.Module):
 
         if self.config.algorithm == "grape":
             self.grape = GRAPE(alpha=config.alpha_grape, gamma=self.gamma, lambd=config.lambd_grape)
-            self.value_network = config.value_network.build()
+            self.value_network = config.value_network.build(hand_encoder=self.config.hand_encoder)
             self.target_value_network = deepcopy(self.value_network)
 
         elif self.config.algorithm == "dsac":
-            self.log_alpha = nn.Parameter(torch.tensor(-3, requires_grad=True, dtype=torch.float32))
+            self.log_alpha = nn.Parameter(torch.tensor(self.config.init_log_alpha, requires_grad=True, dtype=torch.float32))
             self.optimizer_alpha = torch.optim.Adam([self.log_alpha], lr=config.lr_alpha)
-            self.value_network_1 = config.value_network.build()
-            self.value_network_2 = config.value_network.build()
+            self.value_network_1 = config.value_network.build(hand_encoder=self.config.hand_encoder)
+            self.value_network_2 = config.value_network.build(hand_encoder=self.config.hand_encoder)
             self.target_value_network_1 = deepcopy(self.value_network_1)
             self.target_value_network_2 = deepcopy(self.value_network_2)
             self.value_network = TwinQNetwork(self.value_network_1, self.value_network_2)
@@ -140,6 +141,11 @@ class RLMahjong(nn.Module):
         
         self_infos, others_infos, records, global_infos, actions, action_masks, policy_probs, rewards, dones, lengths = batch
 
+        #### DEBUG!!!
+        # for i in reversed(range(len(rewards) - 1)):
+        #     if not dones[i].item():
+        #         dones[i] = dones[i + 1]
+        #         rewards[i] = rewards[i + 1]
         
         one_hot_actions = nn.functional.one_hot(actions.to(torch.int64), num_classes=action_masks.shape[-1]) # convert actions to one-hot
 
@@ -184,13 +190,15 @@ class RLMahjong(nn.Module):
                 # y - alpha * log(pi(a_{t+1}|s_{t+1}))
                 
             if np.random.rand() < 0.01:
+                logging.info("----------- Q 抽查 ------------")
+
                 idx = torch.argwhere((y < -5).to(torch.int) + (y > 5).to(torch.int))
-                if len(idx) > 5:
-                    idx = idx[:5]
-                print("target           ", np.array2string(y[idx].detach().cpu().numpy().flatten(), separator=', ', precision=1))
-                print("q1_a             ", np.array2string(q1[idx, actions[idx]].detach().cpu().numpy().flatten(), separator=', ', precision=1))
-                print("q2_a             ", np.array2string(torch.sum(q2[idx] * one_hot_actions[idx], dim=-1).detach().cpu().numpy().flatten(), separator=', ', precision=1))
-                print("policy_normed_a  ", np.array2string(policy_normed_a[idx].detach().cpu().numpy().flatten(), separator=', ', precision=1))
+                if len(idx) >= 6:
+                    idx = idx[np.random.choice(len(idx), 6)]
+                logging.info("target           " + np.array2string(y[idx].detach().cpu().numpy().flatten(), separator=', ', precision=1))
+                logging.info("q1_a             " + np.array2string(q1[idx, actions[idx]].detach().cpu().numpy().flatten(), separator=', ', precision=1))
+                logging.info("q2_a             " + np.array2string(torch.sum(q2[idx] * one_hot_actions[idx], dim=-1).detach().cpu().numpy().flatten(), separator=', ', precision=1))
+                logging.info("policy_normed_a  " + np.array2string(policy_normed_a[idx].detach().cpu().numpy().flatten(), separator=', ', precision=1))
 
             loss_c_1 = torch.mean(self.mse_loss(torch.sum(q1 * one_hot_actions, dim=-1), y.detach()))
             loss_c_2 = torch.mean(self.mse_loss(torch.sum(q2 * one_hot_actions, dim=-1), y.detach()))
@@ -221,15 +229,15 @@ class RLMahjong(nn.Module):
                 pi_np = pi[rnd_idx].detach().cpu().numpy().reshape([-1])
 
                 if np.random.rand() < 0.01 or pi_np[49] > 0.1:  # contains ron
-                    print("----------- 抽查 ------------")
+                    logging.info("----------- 抽查 ------------")
                     for idx in np.argwhere(action_masks[rnd_idx].detach().cpu().numpy()):
-                        print(action_v2_to_human_chinese[int(idx)] + " policy prob : %3.3f" % (pi_np[int(idx)])
+                        logging.info(action_v2_to_human_chinese[int(idx)] + " policy prob : %3.3f" % (pi_np[int(idx)])
                             + "; logit : %3.3f" % (logits[rnd_idx, int(idx)].detach().cpu().numpy())
                             + "; Q : %3.3f" % (q1[rnd_idx, int(idx)].detach().cpu().numpy()))
                         
-                    print("alpha = {}".format(self.log_alpha.detach().exp().cpu().numpy()))
-                    print("entropy = %3.3f" % (entropy.detach().cpu().numpy().mean()))
-                    print("entropy_old = %3.3f" % (entropy_old.detach().cpu().numpy().mean()))
+                    logging.info("alpha = {}".format(self.log_alpha.detach().exp().cpu().numpy()))
+                    logging.info("entropy = %3.3f" % (entropy.detach().cpu().numpy().mean()))
+                    logging.info("entropy_old = %3.3f" % (entropy_old.detach().cpu().numpy().mean()))
 
                 if self.config.action_mask_mode == 1:
                     eps = torch.tensor(self.config.policy_epsilon, dtype=torch.float32).to(self.device)
@@ -252,33 +260,42 @@ class RLMahjong(nn.Module):
             
             loss_c = self.mse_loss(q[np.arange(rewards.shape[0]), actions], q_grape.detach())
 
+            if np.random.rand() < 0.03:
+                logging.info("----------- Q 抽查 ({}) ------------".format("training" if critic_training else "testing"))
+
+                idx = torch.argwhere((q_grape < -5).to(torch.int) + (q_grape > 5).to(torch.int))
+                if len(idx) >= 6:
+                    idx = idx[np.random.choice(len(idx), 6)]
+                logging.info("target           " + np.array2string(q_grape[idx].detach().cpu().numpy().flatten(), separator=', ', precision=1))
+                logging.info("q_a             " + np.array2string(q[idx, actions[idx]].detach().cpu().numpy().flatten(), separator=', ', precision=1))
             # -------- update actor network ------------
 
-            logits = self.actor_network(self_infos, records, global_infos)[:-1]
-            
-            # from GRAPE ---
-            logpi = torch.log_softmax(logits + action_masks * 100, dim=-1)
-            pi = torch.softmax(logits + action_masks * 100, dim=-1)
+            if actor_training:
+                logits = self.actor_network(self_infos, records, global_infos)[:-1]
+                
+                # from GRAPE ---
+                logpi = torch.log_softmax(logits + action_masks * 100, dim=-1)
+                pi = torch.softmax(logits + action_masks * 100, dim=-1)
 
-            if np.random.rand() < 0.02:
-                print("----------- 抽查 ------------")
-                rnd_idx = np.random.randint(0, int(adv.shape[0]))
-                adv_np = adv[rnd_idx].detach().cpu().numpy().reshape([-1])
-                pi_np = pi[rnd_idx].detach().cpu().numpy().reshape([-1])
-                for idx in np.argwhere(pi_np):
-                    print(action_v2_to_human_chinese[int(idx)] + " policy prob : %4.4f" % (pi_np[int(idx)]) + "; advantage : %5.5f" % (adv_np[int(idx)]))
+                if np.random.rand() < 0.02:
+                    logging.info("----------- 抽查 ------------")
+                    rnd_idx = np.random.randint(0, int(adv.shape[0]))
+                    adv_np = adv[rnd_idx].detach().cpu().numpy().reshape([-1])
+                    pi_np = pi[rnd_idx].detach().cpu().numpy().reshape([-1])
+                    for idx in np.argwhere(pi_np):
+                        logging.info(action_v2_to_human_chinese[int(idx)] + " policy prob : %4.4f" % (pi_np[int(idx)]) + "; advantage : %5.5f" % (adv_np[int(idx)]))
 
-            one_hot_action = nn.functional.one_hot(actions.to(torch.int64), num_classes=pi.shape[-1])  # convert actions to one-hot
+                one_hot_action = nn.functional.one_hot(actions.to(torch.int64), num_classes=pi.shape[-1])  # convert actions to one-hot
 
-            r =  torch.sum((pi / policy_probs.clamp(1e-3, 1)) * one_hot_action, dim=-1)
-            adv_a = torch.sum(adv.detach() * one_hot_action, dim=-1)
-            obj_pi = r * adv_a
+                r =  torch.sum((pi / policy_probs.clamp(1e-3, 1)) * one_hot_action, dim=-1)
+                adv_a = torch.sum(adv.detach() * one_hot_action, dim=-1)
+                obj_pi = r * adv_a
 
-            loss_p = torch.mean(- obj_pi) # policy gradient loss
-            _loss_h = torch.sum(pi * logpi, dim=-1)
-            loss_h = torch.mean(_loss_h)  # entropy loss
+                loss_p = torch.mean(- obj_pi) # policy gradient loss
+                _loss_h = torch.sum(pi * logpi, dim=-1)
+                loss_h = torch.mean(_loss_h)  # entropy loss
 
-            loss_a = loss_p + self.config.coef_entropy * loss_h
+                loss_a = loss_p + self.config.coef_entropy * loss_h
 
         # end computing loss
         
@@ -289,8 +306,8 @@ class RLMahjong(nn.Module):
             if self.algorithm == "dsac":
                 self.optimizer_alpha.zero_grad()
             (loss_a + loss_c).backward()
-            nn.utils.clip_grad_norm_(self.actor_network.parameters(), 10)
-            nn.utils.clip_grad_norm_(self.value_network.parameters(), 10)
+            # nn.utils.clip_grad_norm_(self.actor_network.parameters(), 10)
+            # nn.utils.clip_grad_norm_(self.value_network.parameters(), 10)
             if self.algorithm == "dsac":
                 self.optimizer_alpha.step()
             self.optimizer_a.step()
@@ -299,17 +316,17 @@ class RLMahjong(nn.Module):
         elif actor_training:
             self.optimizer_a.zero_grad()
             loss_a.backward()
-            nn.utils.clip_grad_norm_(self.actor_network.parameters(), 10)
+            # nn.utils.clip_grad_norm_(self.actor_network.parameters(), 10)
             self.optimizer_a.step()
 
         elif critic_training:
             self.optimizer_v.zero_grad()
             loss_c.backward()
-            nn.utils.clip_grad_norm_(self.value_network.parameters(), 10)
+            # nn.utils.clip_grad_norm_(self.value_network.parameters(), 10)
             self.optimizer_v.step()
 
         else:
-            warnings.warn("No training mode specified", UserWarning)
+            warnings.warn("Test mode!", UserWarning)
         
         # self.optimizer_a.zero_grad()
         # self.optimizer_v.zero_grad()
@@ -338,7 +355,7 @@ class RLMahjong(nn.Module):
 
         self.eval()
 
-        return None
+        return loss_c.item()
 
     def reset_q(self):
         self.value_network_1.reset_last_layer()
@@ -396,7 +413,7 @@ class RLMahjong(nn.Module):
             },
             self.checkpoint_dir / f"resume_{self.update_times}.pth" if checkpoint_dir is None else checkpoint_dir / f"resume_{self.update_times}.pth",
         )
-        print(f"Checkpoint saved to {self.checkpoint_dir / f'resume_{self.update_times}.pth' if checkpoint_dir is None else checkpoint_dir / f'resume_{self.update_times}.pth'}", __name__)
+        logging.info(f"Checkpoint saved to {self.checkpoint_dir / f'resume_{self.update_times}.pth' if checkpoint_dir is None else checkpoint_dir / f'resume_{self.update_times}.pth'}", __name__)
         torch.save(
             {
                 "model": self.state_dict(),
@@ -405,13 +422,13 @@ class RLMahjong(nn.Module):
             },
             self.checkpoint_dir / "resume.pth" if checkpoint_dir is None else checkpoint_dir / "resume.pth",
         )
-        print(f"Checkpoint saved to {self.checkpoint_dir / 'resume.pth' if checkpoint_dir is None else checkpoint_dir / 'resume.pth'}", __name__)
+        logging.info(f"Checkpoint saved to {self.checkpoint_dir / 'resume.pth' if checkpoint_dir is None else checkpoint_dir / 'resume.pth'}", __name__)
 
     def _resume(self, checkpoint_dir):
         if checkpoint_dir is not None:
             self.checkpoint_dir = Path(checkpoint_dir)
         if (self.checkpoint_dir / "resume.pth").exists():
-            print(f"Resume from {self.checkpoint_dir / 'resume.pth'}", __name__)
+            logging.info(f"Resume from {self.checkpoint_dir / 'resume.pth'}", __name__)
             checkpoint = torch.load(self.checkpoint_dir / "resume.pth")
             self.load_state_dict(checkpoint["model"])
             # self.optimizer.load_state_dict(checkpoint["optim"])
